7ce3e34b6a810f6320226ab524ae7849755ec7b7
Anchit Bhattacharya <arko@unknowne67e922665e9.home>
2025-03-29 13:14:09 -0700

    marketplace stuff

--- server/app.py
+++ server/app.py
@@ -128,4 +128,4 @@ def logout():
 
 # ------------------- Run the Flask App -------------------
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
